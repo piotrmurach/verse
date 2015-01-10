c6f5336e2b603c0a86e221219110d8037115abac
Piotr Murach <pmurach@gmail.com>
2015-01-10 20:33:09 +0100

    Test verse truncation.

---
+++ spec/unit/truncate_spec.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+RSpec.describe Verse, '#truncate' do
+  it "truncates text" do
+    text = 'ラドクリフ、マラソン五輪代表に1万m出場にも含み'
+    expect(Verse.truncate(text, 10)).to eq('ラドクリフ、マラソ…')
+  end
+end
