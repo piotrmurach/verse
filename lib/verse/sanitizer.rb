72cc31cc9e3083c301ac89e42369777d221aeea7
Piotr Murach <pmurach@gmail.com>
2015-02-28 12:11:39 +0000

    Add sanitizer replace whitespace.

--- lib/verse/sanitizer.rb
+++ lib/verse/sanitizer.rb
@@ -4,15 +4,36 @@ module Verse
   class Sanitizer
     ANSI_MATCHER = /(\[)?\033(\[)?[;?\d]*[\dA-Za-z](\])?/.freeze
 
+    LINE_BREAK = "(\r\n+|\r+|\n+|\t+)".freeze
+
     # Strip ANSI characters from the text
     #
     # @param [String] text
     #
     # @return [String]
     #
-    # @api private
+    # @api public
     def sanitize(text)
       text.gsub(ANSI_MATCHER, '')
+    end
+
+    # Replace separator with whitespace
+    #
+    # @example
+    #   replace(" \n ") # => "  "
+    #   replace("\n")   # => " "
+    #
+    # @param [String] text
+    #
+    # @param [String] separator
+    #
+    # @return [String]
+    #
+    # @api public
+    def replace(text, separator = LINE_BREAK)
+      text.gsub(/([ ]+)#{separator}/, "\\1").
+           gsub(/#{separator}(?<space>[ ]+)/, "\\k<space>").
+           gsub(/#{separator}/, ' ')
     end
   end # Sanitizer
 end # Verse
---
+++ spec/unit/sanitizer/replace_spec.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+RSpec.describe Verse::Sanitizer, '.replace' do
+  subject(:sanitizer) { described_class.new }
+
+  {
+    "  \n"      => '  ',
+    "\n  "      => '  ',
+    "\n"        => ' ',
+    "\n\n\n"    => ' ',
+    " \n "      => '  ',
+    " \n \n \n" => '   '
+  }.each do |string, expected|
+    it "replaces '#{string.gsub(/\n/, '\\n')}' with whitespace" do
+      expect(sanitizer.replace(string)).to eq(expected)
+    end
+  end
+
+  {
+    "  \r\n" => '  ',
+    "\r\n  " => '  ',
+    "\r\n"   => ' ',
+    " \r\n " => '  ',
+  }.each do |string, expected|
+    it "replaces '#{string.gsub(/\r\n/, '\\r\\n')}' with whitespace" do
+      expect(sanitizer.replace(string, "\r\n")).to eq(expected)
+    end
+  end
+end
